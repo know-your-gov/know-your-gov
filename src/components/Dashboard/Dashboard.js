93603be768d5e152850088f3012ddbce76f15417
sam-obembe <seyanuobembe@gmail.com>
2019-03-18 15:46:21 -0500

    started work on Dashboard component. Created representative component for dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import Representative from './Representative'
+
 
 export class Dashboard extends Component {
+  constructor(){
+    super()
+    this.state = {
+      senateRep: {
+        title: "Senator",
+        pic: "http://www4.pictures.zimbio.com/gi/Beto+O+Rourke+Running+Beto+Premiere+2019+SXSW+Ao4bSmRkkL6l.jpg",
+        name: "Beto",
+        contact: "SomeStuff",
+      },
+      congressRep: {
+        title: "Representative",
+        pic: "https://jacksonlee.house.gov/sites/jacksonlee.house.gov/files/wysiwyg_uploaded/Photograph%20of%20Congresswoman.JPG",
+        name: "Sheila Jackson Lee",
+        contact: "some stuff"
+      }
+    }
+  }
+
   render() {
-    return <div>Dashboard</div>;
+    return (
+      <div style = {{display: "flex", justifyContent: "space-around"}}>
+        {/* welcome card */}
+        <div style = {{width: "30vw", height: "40vh", backgroundColor: "grey"}}>
+          <Card>
+            <CardContent>
+              <Typography variant = "h4">Welcome insert userName here</Typography>
+            </CardContent>
+          </Card>
+        </div>
+
+        {/* card showing senate representatitve */}
+        <Representative repDets = {this.state.senateRep}/>
+        <Representative repDets = {this.state.congressRep}/>
+
+      </div>
+
+    );
   }
 }
 
---
+++ src/components/Dashboard/Representative.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {withStyles} from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+const styles = {
+  media:{
+    width: "25vw"
+  },
+  root: {
+    width: "30vw"
+  }
+}
+
+
+const Representative =(props)=>{
+  const {classes} = props
+  const dets = props.repDets
+  return(
+    <div>
+      <Card className = {classes.root}>
+        {/* representative picture */}
+        <CardActionArea>
+          <CardMedia className = {classes.media} component = "img" src = {dets.pic} title = "beto"/>
+        </CardActionArea>
+
+        {/* representative contact details */}
+        <CardContent>
+          <Typography variant = "h4">{dets.title} {dets.name}</Typography>
+          <p>Contact Details: {dets.contact}</p> 
+        </CardContent>
+
+        <CardActions>
+          <Button>See more</Button>
+        </CardActions>
+      </Card>
+
+    </div>
+  )
+}
+
+Representative.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Representative)
